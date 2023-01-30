19a6f691fc88843f2ae90b2315d651b51e5a73ef
Hessam <hessam2koucheki@gmail.com>
2023-01-31 01:46:56 +0330

    perform and compare AES & DES

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import json
 from Crypto.Cipher import AES
 import time
 from conf import filename, key
-from Encryptor import AES_Encryptor
+from Encryptor import AES_Encryptor, DES_Encryptor
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,14 +23,34 @@ try:
     
     # Send encrypted data to server
     sock.sendall(open(enc_file_name, 'rb').read())
-    print('Sent encrypted file')
+    print('Sent AES encrypted file')
     
     elapsed_time = time.time() - start_time
-    print("Time elapsed for AES to encrypt and send:", elapsed_time, "seconds")
+    print("--- Time elapsed for AES to encrypt and send:", elapsed_time, "seconds")
     #================ AES ==================
     #=======================================
+    # Refresh the connection
+    print('=' * 20)
+    sock.close()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(server_address)
+    print('=' * 20)
+    #=======================================
     #================ DES ==================
+
+    print('Running DES...')
+    start_time = time.time()
+    # Encrypt file
+    enc = DES_Encryptor()
+    enc_file_name = enc.encrypt_file(filename)
     
+    # Send encrypted data to server
+    sock.sendall(open(enc_file_name, 'rb').read())
+    print('Sent DES encrypted file')
+    
+    elapsed_time = time.time() - start_time
+    print("--- Time elapsed for DES to encrypt and send:", elapsed_time, "seconds")
+    #================ DES ==================
 
 
 finally:  
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import os
 from Crypto.Cipher import AES
 from conf import filename, key
-from Encryptor import AES_Encryptor
+from Encryptor import AES_Encryptor, DES_Encryptor
 import time
 
 # Create a TCP/IP socket
@@ -25,26 +25,47 @@ try:
     start_time = time.time()
 
     enc = AES_Encryptor()
-
     # Receive the data in small chunks 
-    with open('rec_' + filename, 'wb') as f:
+    with open('rec_' + filename+ '.AES.enc', 'wb') as f:
         while True:
             data = connection.recv(1024)
             if not data: break
             # write data to a file
             f.write(data)
-        print('file recieved!')
+        print('AES file recieved!')
         
-    enc.decrypt_file('rec_' + filename)
-    print('file decrypted!')
+    enc.decrypt_file('rec_' + filename + '.AES.enc')
+    print('AES file decrypted!')
     elapsed_time = time.time() - start_time
-    print("Time elapsed for AES to recieve and decrypt:", elapsed_time, "seconds")
+    print("--- Time elapsed for AES to recieve and decrypt:", elapsed_time, "seconds")
     #================ AES ==================
     #=======================================
+    # Get a new connection
+    print('=' * 20)
+    connection, client_address = sock.accept()
+    print('=' * 20)
+    #=======================================
+    #================ DES ==================
+    start_time = time.time()
+
+    des_enc = DES_Encryptor()
+    # Receive the data in small chunks 
+    filename_to_dec = 'rec_' + filename + '.DES.enc'
+    with open(filename_to_dec, 'wb') as f:
+        while True:
+            data = connection.recv(1024)
+            if not data: break
+            # write data to a file
+            f.write(data)
+        print('DES file recieved!')
+    
+    # print(filename_to_dec := 'rec_' + filename + '.DES.enc')
+    des_enc.decrypt_file(filename_to_dec)
+    print('DES file decrypted!')
+    elapsed_time = time.time() - start_time
+    print("--- Time elapsed for DES to recieve and decrypt:", elapsed_time, "seconds")
     #================ DES ==================
     
-
-
 finally:
     # Clean up the connection
     print('Closing connection')  
